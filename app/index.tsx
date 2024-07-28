9670ffb6c4211be1af81743c3736f1035bb3b0e7
AlexJoaquimPereira <alexpereira1781@gmail.com>
2024-07-29 00:35:40 +0530

    Index page Fully completed the page with working links to other pages Any addition will be seen later

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { Image, StyleSheet, View, Text } from "react-native";
-import Button from "@/components/ConnectifyButton"
+import Button from "@/components/ConnectifyButton";
+import { Link } from 'expo-router'
 
-export default function Index({navigation}) {
+const Index = () => {
   return (
-    //<SafeAreaView>
     <View style={styles.background}>
       <View style={styles.logoContainer}>
-        <Image source={require("@/assets/images/connectify-logo.png")} style={{width: 120, height: 120}}/>
+        <Image source={require("@/assets/images/connectify-logo.png")} style={{ width: 120, height: 120 }} />
         <Text style={styles.text}>ConnectiFy</Text>
       </View>
       <View style={styles.buttonArea}>
-        <Button backgroundColor='#A98CE6' textColor='white' onPress={() => {navigation.navigate('SignUp')}}>
-          Sign Up
-        </Button>
+        <Link href="/signup" asChild>
+          <Button backgroundColor='#A98CE6' textColor='white'>
+            Sign Up
+          </Button>
+        </Link>
       </View>
       <View style={styles.buttonArea}>
-        <Button backgroundColor='#F0F1F1' textColor='black' onPress={() => {navigation.navigate('LogIn')}}>
+      <Link href="/login" asChild>
+        <Button backgroundColor='#F0F1F1' textColor='black'>
           Log In
         </Button>
+        </Link>
       </View>
     </View>
-    //</SafeAreaView>
   );
 }
 
@@ -47,3 +50,5 @@ const styles = StyleSheet.create({
     color: 'black',
   },
 });
+
+export default Index;
