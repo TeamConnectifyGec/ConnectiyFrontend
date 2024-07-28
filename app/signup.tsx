0a603dc2b7289750807f306a649cafc688a0c1e9
AlexJoaquimPereira <alexpereira1781@gmail.com>
2024-07-29 00:37:18 +0530

    Sign up page Completed the frontend of the signup page, only waiting for the backend dev to complete his work

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, StyleSheet, View, Text } from "react-native";
 import Button from "@/components/ConnectifyButton"
+import CfyTextInput from '@/components/ConnectifyTextInput';
 import { Link } from "expo-router"
 
-export default function SignUp() {
+const SignUp = () => {
+  const [email, setEmail] = useState();
+  const [username, setUsername] = useState();
+  const [recheckpassword, setRecheckPassword] = useState();
+  const [password, setPassword] = useState();
   return (
-    //<SafeAreaView>
     <View style={styles.background}>
       <View style={styles.logoContainer}>
-        <Image source={require("@/assets/images/connectify-logo.png")} style={{width: 120, height: 120}}/>
-        <Text style={styles.text}>ConnectiFy</Text>
+        <Image source={require("@/assets/images/connectify-logo.png")} style={{ width: 120, height: 120 }} />
       </View>
-      <View style={styles.buttonArea}>
-        <Button backgroundColor='#A98CE6' textColor='white' onPress={() => { }}>
-          Sign Up
-        </Button>
-      </View>
-      <View style={styles.buttonArea}>
-        <Link href={"@/app/inde.tsx"} asChild>
-        <Button backgroundColor='#F0F1F1' textColor='black' onPress={() => {"@/app/ind.tsx"}}>
-          Log In
-        </Button>
-        </Link>
+      <View>
+        <CfyTextInput
+          placeholder="Set email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        <CfyTextInput
+          placeholder="Set username"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <CfyTextInput
+          placeholder="Set password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <CfyTextInput
+          placeholder="Re-enter password"
+          value={recheckpassword}
+          onChangeText={setRecheckPassword}
+          secureTextEntry
+        />
+        <View style={styles.buttonArea}>
+          <Text>(Capcha will come here)</Text>
+        </View>
+        <View style={styles.buttonArea}>
+          <Button backgroundColor='#A98CE6' textColor='white' onPress={() => { }}>
+            Sign Up
+          </Button>
+        </View>
+        <View style={styles.buttonArea}>
+          <Text style={styles.text}>Already have an account?</Text>
+          <Link href='/login' style={{color: '#A98CE6',fontWeight: 'bold'}}>
+            Login
+          </Link>
+        </View>
       </View>
     </View>
-    //</SafeAreaView>
   );
 }
 
@@ -36,17 +65,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   buttonArea: {
-    paddingTop: 15,
+    paddingTop: 10,
+    alignItems: 'center'
   },
   logoContainer: {
-    alignItems: 'center'
+    alignItems: 'center',
   },
   text: {
     fontFamily: 'Inter',
-    fontSize: 20,
+    fontSize: 15,
     lineHeight: 21,
-    fontWeight: 'bold',
+    //fontWeight: 'bold',
     letterSpacing: 0.25,
     color: 'black',
   },
 });
+
+export default SignUp;
