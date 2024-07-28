1f20d9cb0f16c488657e2aea302a04e8e78e80c9
AlexJoaquimPereira <alexpereira1781@gmail.com>
2024-07-28 16:10:27 +0530

    Completed start page, need to figure out linking of pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { Image, StyleSheet, View, Text } from "react-native";
+import Button from "@/components/ConnectifyButton"
 
-export default function Index() {
+export default function Index({navigation}) {
   return (
+    //<SafeAreaView>
     <View style={styles.background}>
-      <View>
-        <Text>Add logo here</Text>
+      <View style={styles.logoContainer}>
+        <Image source={require("@/assets/images/connectify-logo.png")} style={{width: 120, height: 120}}/>
+        <Text style={styles.text}>ConnectiFy</Text>
+      </View>
+      <View style={styles.buttonArea}>
+        <Button backgroundColor='#A98CE6' textColor='white' onPress={() => {navigation.navigate('SignUp')}}>
+          Sign Up
+        </Button>
+      </View>
+      <View style={styles.buttonArea}>
+        <Button backgroundColor='#F0F1F1' textColor='black' onPress={() => {navigation.navigate('LogIn')}}>
+          Log In
+        </Button>
       </View>
     </View>
+    //</SafeAreaView>
   );
 }
 
@@ -17,7 +32,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
-
-  }
+  buttonArea: {
+    paddingTop: 15,
+  },
+  logoContainer: {
+    alignItems: 'center'
+  },
+  text: {
+    fontFamily: 'Inter',
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+  },
 });
