af18c82a6eaf2f4baa6778b5ba63e0b064c691cc
AlexJoaquimPereira <alexpereira1781@gmail.com>
2024-07-29 00:38:59 +0530

    Login page Completed the frontend of the login page, only waiting for the backend dev to complete his work

--- app/login.tsx
+++ app/login.tsx
@@ -1,23 +1,67 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, View, Text } from "react-native";
+import Button from "@/components/ConnectifyButton"
+import CfyTextInput from '@/components/ConnectifyTextInput';
+import { Link } from "expo-router"
 
-export default function AnotherPage() {
+const LogIn = () => {
+  const [username, extractUsername] = useState();
+  const [password, extractPassword] = useState();
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is another page!</Text>
+    <View style={styles.background}>
+      <View style={styles.logoContainer}>
+        <Image source={require("@/assets/images/connectify-logo.png")} style={{ width: 120, height: 120 }} />
+      </View>
+      <View>
+        <CfyTextInput
+          placeholder="Username"
+          value={username}
+          onChangeText={extractUsername}
+        />
+        <CfyTextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={extractPassword}
+          secureTextEntry
+        />
+        <View style={styles.buttonArea}>
+          <Button backgroundColor='#A98CE6' textColor='white' onPress={() => { }}>
+            Log In
+          </Button>
+        </View>
+        <View style={styles.buttonArea}>
+          <Text style={styles.text}>Do not have an account?</Text>
+          <Link href='/signup' style={{color: '#A98CE6',fontWeight: 'bold'}}>
+            Sign Up
+          </Link>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: 'white',
     alignItems: 'center',
-    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
+  },
+  buttonArea: {
+    paddingTop: 10,
+    alignItems: 'center'
+  },
+  logoContainer: {
+    alignItems: 'center',
   },
   text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontFamily: 'Inter',
+    fontSize: 15,
+    lineHeight: 21,
+    //fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
   },
 });
+
+export default LogIn;
